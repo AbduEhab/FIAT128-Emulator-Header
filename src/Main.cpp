259c9e32f9eb831c305c09b8aff4fa2c81ac90be
Moataz-0 <moatazm43@gmail.com>
2022-11-18 17:17:01 +0200

    makes-->make

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,7 +4,7 @@ int main([[maybe_unused]] int, [[maybe_unused]] char **)
 {
     Instrumentor::Get().beginSession("Main func");
 
-    TimedBlock block("Mains function");
+    TimedBlock block("Main function");
 
     print_by_force("Hello World!\nThe largest number I can hold is: ", std::numeric_limits<int>::max());
     print_by_force("\nPress enter to continue...");
